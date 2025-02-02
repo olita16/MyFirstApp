93d05d1e9c15b7a5b4c3d5e83d792356ad035b38
olita16 <119816415+olita16@users.noreply.github.com>
2025-02-02 22:52:14 +0100

    change components, create and add icons from react-native-svg, add login screen, add colors to global styles, correct registration screen, add expo-image-picker

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,13 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import RegistrationScreen from './src/screens/RegistrationScreen';
-import HomeIndicator from './src/components/HomeIndicator';
+
+import { useState } from "react";
+import { useFonts } from "expo-font";
+import LoginScreen from "./src/screens/LoginScreen";
+import RegistrationScreen from "./src/screens/RegistrationScreen";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 
 export default function App() {
-  return (
-    <>
-      <RegistrationScreen />
-      <StatusBar style='auto' />
-      <HomeIndicator />
-    </>
+  const [isLoginScreen, setIsLoginScreen] = useState(false);
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto_Condensed-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto_Condensed-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto_Condensed-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={style.section}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return isLoginScreen ? (
+    <LoginScreen onRegister={() => setIsLoginScreen(false)} />
+  ) : (
+    <RegistrationScreen onHasAccount={() => setIsLoginScreen(true)} />
   );
 }
+
+const style = StyleSheet.create({
+  section: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
